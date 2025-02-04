61b0058850a697b5ebd76c36260a790b224bb2f2
Taher Rhandi <taher.rhandi@etudiant.univ-brest.fr>
2025-02-04 10:44:08 +0100

    init backend

---
+++ backend/src/main/java/com/example/backendagile/BackendAgileApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendagile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAgileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendAgileApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/backendagile/BackendAgileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendagile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAgileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
