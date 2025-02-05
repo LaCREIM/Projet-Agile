82addf6c536c00b28b085837bba0c54576bc5946
Sohaib Manah <sohaib.manah@etudiant.univ-brest.fr>
2025-02-05 15:28:53 +0100

    refactor Promotion entity

---
+++ backend/src/main/java/com/example/backendagile/dto/PromotionDTO.java
@@ -0,0 +1,12 @@
+package com.example.backendagile.dto;
+
+import java.time.LocalDate;
+
+public class PromotionDTO {
+    private String codeFormation;
+    private String anneeUniversitaire;
+    private LocalDate dateRentree;
+    private String lieuRentree;
+    private String processusStage;
+    private String commentaire;
+}
