c8275fe3babfa01f67450aef64a8f81cac4f9b15
Sohaib Manah <sohaib.manah@etudiant.univ-brest.fr>
2025-02-06 16:53:19 +0100

    add dto to Enseignant and Etudiant

---
+++ backend/src/main/java/com/example/backendagile/dto/EnseignantDTO.java
@@ -0,0 +1,21 @@
+package com.example.backendagile.dto;
+
+import lombok.Data;
+
+@Data
+public class EnseignantDTO {
+    private Long id;
+    private String nom;
+    private String prenom;
+    private String adresse;
+    private String sexe;
+    private String ville;
+    private String pays;
+    private String mobile;
+    private String telephone;
+    private String codePostal;
+    private String type;
+    private String emailUbo;
+    private String emailPerso;
+    private String password;
+}
