fab05bbdb3f1118a87261b782da47e770a393b22
Sohaib Manah <sohaib.manah@etudiant.univ-brest.fr>
2025-02-14 15:16:03 +0100

    feat: enhance Etudiant creation test and add pagination for standard questions

--- backend/src/main/java/com/example/backendagile/repositories/QuestionRepository.java
+++ backend/src/main/java/com/example/backendagile/repositories/QuestionRepository.java
@@ -1,14 +1,26 @@
 package com.example.backendagile.repositories;
 
 import com.example.backendagile.entities.Question;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface QuestionRepository extends JpaRepository<Question, Long> {
+public interface QuestionRepository extends JpaRepository<Question, Long>, PagingAndSortingRepository<Question, Long> {
     @Query("SELECT q FROM Question q WHERE q.type = 'QUS' AND q.noEnseignant IS NULL")
     List<Question> findStandardQuestions();
+
+    @Query(value = """
+                SELECT * FROM (
+                    SELECT q.*, ROWNUM rnum FROM (
+                        SELECT * FROM question WHERE type = 'QUS' AND no_enseignant IS NULL
+                    ) q WHERE ROWNUM <= :endRow
+                ) WHERE rnum > :startRow
+            """, nativeQuery = true)
+    List<Question> findAllWithPagination(@Param("startRow") int startRow, @Param("endRow") int endRow);
 }
