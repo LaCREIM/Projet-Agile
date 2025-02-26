67f39cc8724c69cf269b3a2d56803ae266af2637
Sohaib Manah <sohaib.manah@etudiant.univ-brest.fr>
2025-02-26 15:13:43 +0100

    feat: add authentication check and logout functionality in App and NavBar components

--- client/src/components/Admin/NavBar.tsx
+++ client/src/components/Admin/NavBar.tsx
@@ -1,14 +1,26 @@
 import ubo from "/ubo.png";
-import { FiLogOut } from "react-icons/fi";
+import {FiLogOut} from "react-icons/fi";
+import {useNavigate} from "react-router-dom";
+
 const NavBar = () => {
-  return (
-    <div className="navbar bg-base-100 rounded-b-md shadow-md px-[2%] flex justify-between flex-row">
-      <img src={ubo} className="w-20" alt="ubo-logo" />
-      <a className="navbar-end text-xl hover:cursor-pointer hover:bg-gray-100 btn bg-white w-fit p-2 rounded-full">
-        <FiLogOut size={28} />
-      </a>
-    </div>
-  );
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        navigate("/");
+    };
+
+    return (
+        <div className="navbar bg-base-100 rounded-b-md shadow-md px-[2%] flex justify-between flex-row">
+            <img src={ubo} className="w-20" alt="ubo-logo"/>
+            <a
+                className="navbar-end text-xl hover:cursor-pointer hover:bg-gray-100 btn bg-white w-fit p-2 rounded-full"
+                onClick={handleLogout}
+            >
+                <FiLogOut size={28}/>
+            </a>
+        </div>
+    );
 };
 
 export default NavBar;
