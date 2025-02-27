b418e9938d9bc168c3a7dd2ac97d78d27a5e5235
Taher Rhandi <taher.rhandi@etudiant.univ-brest.fr>
2025-02-27 08:44:56 +0100

    ordre question in rubrique

--- backend/src/main/java/com/example/backendagile/repositories/RubriqueQuestionStdRepository.java
+++ backend/src/main/java/com/example/backendagile/repositories/RubriqueQuestionStdRepository.java
@@ -4,10 +4,12 @@ import com.example.backendagile.entities.RubriqueQuestion;
 import com.example.backendagile.entities.RubriqueQuestionId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 @Repository
 public interface RubriqueQuestionStdRepository extends JpaRepository<RubriqueQuestion, RubriqueQuestionId> {
+    @Query("SELECT rq FROM RubriqueQuestion rq WHERE rq.idRubrique.id = ?1 ORDER BY rq.ordre ASC")
     List<RubriqueQuestion> findByIdRubriqueId(Long idRubrique);
 
 }
