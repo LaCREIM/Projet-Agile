6d6f0caf338e322af691e20fab69b1dee05122ae
YassineIDR <yassineidrissi2005@gmail.com>
2025-02-27 08:55:35 +0100

    finished rubrique

--- client/src/api/ApiSlice.ts
+++ client/src/api/ApiSlice.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseQuery = fetchBaseQuery({
     //baseUrl: "http://localhost:8080",
-    baseUrl: "https://652a-2a04-cec0-109b-4f22-4002-928b-2789-2c5f.ngrok-free.app",
+    baseUrl: "https://7269-2a04-cec0-109e-ba57-3106-e42e-ffe3-cd4c.ngrok-free.app",
 
 });
 export const apiSlice = createApi({
