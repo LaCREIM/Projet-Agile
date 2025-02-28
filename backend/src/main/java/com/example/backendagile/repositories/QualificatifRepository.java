3c565790d1f86c48e2aeb1dea6f6dff271b7917d
Taher Rhandi <taher.rhandi@etudiant.univ-brest.fr>
2025-02-28 10:50:07 +0100

    gestion erreur Qualificatif v1.0

--- backend/src/main/java/com/example/backendagile/repositories/QualificatifRepository.java
+++ backend/src/main/java/com/example/backendagile/repositories/QualificatifRepository.java
@@ -1,6 +1,7 @@
 package com.example.backendagile.repositories;
 import com.example.backendagile.entities.Qualificatif;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -8,4 +9,7 @@ import java.util.Optional;
 @Repository
 public interface QualificatifRepository extends JpaRepository<Qualificatif,Long> {
     Optional<Qualificatif> findById(Long id);
+
+    @Query("SELECT q FROM Qualificatif q WHERE q.minimal = ?1 AND q.maximal = ?2")
+    Qualificatif findByDesignation(String minimal , String maximal);
 }
