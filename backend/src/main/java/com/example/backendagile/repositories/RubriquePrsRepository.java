8a1042e44172bfa0882c87d48ddf3d42d5fd703a
Taher Rhandi <taher.rhandi@etudiant.univ-brest.fr>
2025-03-03 16:18:52 +0100

    search with pagination RubriquePrs

--- backend/src/main/java/com/example/backendagile/repositories/RubriquePrsRepository.java
+++ backend/src/main/java/com/example/backendagile/repositories/RubriquePrsRepository.java
@@ -3,6 +3,7 @@ package com.example.backendagile.repositories;
 import com.example.backendagile.entities.Rubrique;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +11,11 @@ public interface RubriquePrsRepository extends JpaRepository<Rubrique, Long> {
     @Query("SELECT r FROM Rubrique r WHERE r.noEnseignant.id = :noEnseignant")
     List<Rubrique> findByNoEnseignant_NoEnseignant(Long noEnseignant);
 
+    @Query("SELECT r FROM Rubrique r WHERE r.noEnseignant.id = :noEnseignant  and LOWER(r.designation) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Rubrique> searchRubriqueWithPagination( @Param("keyword") String keyword,
+                                                 @Param("noEnseignant") Long noEnseignant,
+                                                 @Param("startRow") int startRow,
+                                                 @Param("endRow") int endRow);
+
+    long countByDesignationContainingIgnoreCase(String designation);
 }
