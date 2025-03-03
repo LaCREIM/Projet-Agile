324f3ed648ced497f690a5ec1ee08671d8c54e1c
Nada El baghdadi <nada.elbaghdadi@etu.uae.ac.ma>
2025-03-03 15:16:49 +0100

    search avec pagination

--- backend/src/main/java/com/example/backendagile/repositories/QualificatifRepository.java
+++ backend/src/main/java/com/example/backendagile/repositories/QualificatifRepository.java
@@ -2,9 +2,13 @@ package com.example.backendagile.repositories;
 import com.example.backendagile.entities.Qualificatif;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 @Repository
 public interface QualificatifRepository extends JpaRepository<Qualificatif,Long> {
@@ -12,4 +16,27 @@ public interface QualificatifRepository extends JpaRepository<Qualificatif,Long>
 
     @Query("SELECT q FROM Qualificatif q WHERE q.minimal = ?1 AND q.maximal = ?2")
     Qualificatif findByDesignation(String minimal , String maximal);
+
+
+ // List<Qualificatif> findByMinimalContainingIgnoreCaseOrMaximalContainingIgnoreCase(String minimal, String maximal);
+   @Query(value = """
+        SELECT * FROM (
+            SELECT q.*, ROWNUM rnum FROM (
+                SELECT * FROM QUALIFICATIF 
+                WHERE UPPER(MINIMAL) LIKE UPPER(:keyword) 
+                   OR UPPER(MAXIMAL) LIKE UPPER(:keyword)
+                ORDER BY MINIMAL
+            ) q WHERE ROWNUM <= :endRow
+        ) WHERE rnum > :startRow
+        """, nativeQuery = true)
+List<Qualificatif> searchWithPagination(
+        @Param("keyword") String keyword,
+        @Param("startRow") int startRow,
+        @Param("endRow") int endRow);
+
+
+ long countByMinimalContainingIgnoreCaseOrMaximalContainingIgnoreCase(String minimal, String maximal);
+
+
+   
 }
