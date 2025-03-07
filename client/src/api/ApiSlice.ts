d66ea1206ead1be076f61777fa3ce6e0fdad70b8
Sohaib Manah <sohaib.manah@etudiant.univ-brest.fr>
2025-03-07 10:10:09 +0100

    feat: refactor API base URL configuration for improved maintainability

--- client/src/api/ApiSlice.ts
+++ client/src/api/ApiSlice.ts
@@ -1,12 +1,14 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+import {API_BASE_URL} from "./baseUrl.ts";
+
 const baseQuery = fetchBaseQuery({
 
-    baseUrl: "https://dc8a-2a04-cec0-1027-7f74-c1ca-8a7f-b1e5-6f85.ngrok-free.app",
-     //baseUrl: "http://localhost:8080",
+    baseUrl: API_BASE_URL,
+    //baseUrl: "http://localhost:8080",
 
 });
 export const apiSlice = createApi({
     baseQuery: baseQuery,
     endpoints: () => ({}),
-    
+
 });
--- client/src/api/axiosConfig.ts
+++ client/src/api/axiosConfig.ts
@@ -1,20 +1,10 @@
 import axios from "axios";
-
-
-
-
-
-export const API_BASE_URL = "https://dc8a-2a04-cec0-1027-7f74-c1ca-8a7f-b1e5-6f85.ngrok-free.app/api";
-//  export const API_BASE_URL = "http://localhost:8080/api/";
-
-
-
-
+import {API_BASE_URL} from "./baseUrl.ts";
 
 
 
 const axiosInstance = axios.create({
-    baseURL: API_BASE_URL,
+    baseURL: API_BASE_URL + "/api",
     headers: {
         "Content-Type": "application/json",
         "Authorization": `Bearer ${localStorage.getItem("token")}`,
