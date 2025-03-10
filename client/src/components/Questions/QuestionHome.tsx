d286a5763b39018ce9da8c3462e1b10997f7f5c5
Sohaib Manah <sohaib.manah@etudiant.univ-brest.fr>
2025-03-10 11:43:09 +0100

    feat: enhance QuestionHome component with role-based question modification restrictions and layout improvements

--- client/src/components/Questions/QuestionHome.tsx
+++ client/src/components/Questions/QuestionHome.tsx
@@ -1,21 +1,18 @@
-import { useEffect, useState } from "react";
-import { useAppDispatch, useAppSelector } from "../../hook/hooks";
+import {useEffect, useState} from "react";
+import {useAppDispatch, useAppSelector} from "../../hook/hooks";
 import AddQuestion from "./AddQuestion";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
-import {
-  fetchQuestionsAsync,
-  // getQuestionPersoAsync,
-} from "../../features/QuestionSlice";
-import { Qualificatif, Question } from "../../types/types";
-import { ToastContainer } from "react-toastify";
-import { RootState } from "../../api/store";
-import UpdateQuestion from "./UpdateQuestion";
-import { fetchQualificatifsAsync } from "../../features/QualificatifSlice";
-import DeleteQuestionConfirmation from "./DeleteQuestionConfirmation";
-import { typeQuestionMapper } from "../../mappers/mappers";
+import {fetchQuestionsAsync,} from "../../features/QuestionSlice";
+import {Qualificatif, Question} from "../../types/types";
+import {RootState} from "../../api/store";
+import {fetchQualificatifsAsync} from "../../features/QualificatifSlice";
+import {typeQuestionMapper} from "../../mappers/mappers.ts";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPenToSquare, faTrash} from "@fortawesome/free-solid-svg-icons";
+import UpdateQuestion from "./UpdateQuestion.tsx";
+import DeleteQuestionConfirmation from "./DeleteQuestionConfirmation.tsx";
 
 const QuestionHome = () => {
+  const role = localStorage.getItem('role');
   document.title = "UBO | Questions";
   const dispatch = useAppDispatch();
   const questions = useAppSelector(
@@ -76,7 +73,6 @@ const QuestionHome = () => {
 
   return (
     <>
-      <ToastContainer theme="colored" />
       <div className="flex flex-col gap-5 items-center pt-32 mx-auto rounded-s-3xl bg-white w-full h-screen">
         <h1 className="text-xl">Liste des questions</h1>
         <div className="flex flex-row items-center justify-end gap-5 w-[60%] px-14">
@@ -90,8 +86,8 @@ const QuestionHome = () => {
           </div>
         </div>
 
-        <div className="overflow-y-auto ">
-          <table className="table table-zebra w-fit">
+        <div className="overflow-y-auto w-full">
+          <table className="table table-zebra w-[75%] mx-auto">
             <thead>
               <tr>
                 <th>Intitulé</th>
@@ -111,65 +107,83 @@ const QuestionHome = () => {
                   </td>
                 </tr>
               ) : (
-                paginatedQuestions.map((question: Question, index: number) => (
-                  <tr
-                    key={question.id}
-                    className=" transition-all duration-75 "
-                  >
-                    <td className="px-4 py-2 w-[20%]">
-                      {question.intitule || "N/A"}
-                    </td>
-                    <td className="px-4 py-2">
-                      {question?.idQualificatif?.maximal +
-                        " - " +
-                        question?.idQualificatif?.minimal || "N/A"}
-                    </td>
-                    <td className="px-4 py-2 ">
-                      {question.type === "QUP" ? (
-                        <div className="badge badge-accent text-white">
-                          {typeQuestionMapper(question.type)}
-                        </div>
-                      ) : (
-                        <div className="badge badge-success text-white">
-                          {typeQuestionMapper(question.type)}
-                        </div>
-                      )}
-                    </td>
+                  paginatedQuestions.map((question: Question, index: number) => {
+                    // sorry mom , i had to do this
+                    const isEnseneigentQuestionPerso = role == "ENS" && question.type === "QUS";
+                    return (
+                        <tr
+                            key={question.id}
+                            className=" transition-all duration-75 "
+                        >
+                          <td className="px-4 py-2 w-[20%]">
+                            {question.intitule || "N/A"}
+                          </td>
+                          <td className="px-4 py-2">
+                            {question?.idQualificatif?.maximal +
+                                " - " +
+                                question?.idQualificatif?.minimal || "N/A"}
+                          </td>
+                          <td className="px-4 py-2 ">
+                            {question.type === "QUP" ? (
+                                <div className="badge badge-accent text-white">
+                                  {typeQuestionMapper(question.type)}
+                                </div>
+                            ) : (
+                                <div className="badge badge-success text-white">
+                                  {typeQuestionMapper(question.type)}
+                                </div>
+                            )}
+                          </td>
 
-                    <td className="flex gap-3 justify-center items-center">
-                      <FontAwesomeIcon
-                        icon={faPenToSquare}
-                        className="text-black text-base cursor-pointer"
-                        onClick={() => {
-                          handleClickUpdate(index);
-                          openModal(`updateQuestion-${index}`);
-                        }}
-                      />
-                      <FontAwesomeIcon
-                        icon={faTrash}
-                        className="text-black text-base cursor-pointer"
-                        onClick={() => openModal(`delete-${index}`)}
-                      />
-                    </td>
+                          <td className="flex gap-3 justify-center items-center">
+                            <div className="tooltip"
+                                 data-tip={isEnseneigentQuestionPerso ? "Vous ne pouvez pas modifier une question standard" : "Modifier"}>
+                              <FontAwesomeIcon
+                                  icon={faPenToSquare}
+                                  className={`text-black text-base cursor-pointer ${isEnseneigentQuestionPerso ? "text-gray-400 cursor-not-allowed" : ''}`}
+                                  onClick={() => {
+                                    if (!isEnseneigentQuestionPerso) {
+                                      handleClickUpdate(index);
+                                      openModal(`updateQuestion-${index}`);
+                                    }
+                                  }}
+                              />
+                            </div>
+                            <div className="tooltip"
+                                 data-tip={isEnseneigentQuestionPerso ? "Vous ne pouvez pas supprimer une question standard" : "Supprimer"}>
+                              <FontAwesomeIcon
+                                  icon={faTrash}
+                                  className={`text-black text-base cursor-pointer ${isEnseneigentQuestionPerso ? "text-gray-400 cursor-not-allowed" : ''}`}
+                                  onClick={() => {
+                                    if (!isEnseneigentQuestionPerso) {
+                                      openModal(`delete-${index}`);
+                                    }
+                                  }}
+                              />
+                            </div>
+                          </td>
 
-                    {/* Modal pour la mise à jour de la question */}
-                    <dialog id={`updateQuestion-${index}`} className="modal">
-                      <UpdateQuestion
-                        questionData={question}
-                        qualificatifs={qualificatifs}
-                        onClose={() => closeModal(`updateQuestion-${index}`)} // Fermer le modal
-                      />
-                    </dialog>
+                          {/* Modal pour la mise à jour de la question */}
+                          <dialog id={`updateQuestion-${index}`} className="modal">
+                            <UpdateQuestion
+                                questionData={question}
+                                qualificatifs={qualificatifs}
+                                onClose={() => closeModal(`updateQuestion-${index}`)} // Fermer le modal
+                            />
+                          </dialog>
 
-                    {/* Modal pour la suppression de la question */}
-                    <dialog id={`delete-${index}`} className="modal">
-                      <DeleteQuestionConfirmation
-                        question={question}
-                        currentPage={currentPage}
-                      />
-                    </dialog>
-                  </tr>
-                ))
+                          {/* Modal pour la suppression de la question */}
+                          <dialog id={`delete-${index}`} className="modal">
+                            <DeleteQuestionConfirmation
+                                question={question}
+                                currentPage={currentPage}
+                            />
+                          </dialog>
+                        </tr>
+                  )
+
+                  }
+                  )
               )}
             </tbody>
           </table>
