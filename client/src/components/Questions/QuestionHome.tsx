d8779a6f250dda1160d3e1fa8add1cf6d701c2a7
Sohaib Manah <sohaib.manah@etudiant.univ-brest.fr>
2025-03-10 14:03:47 +0100

    feat: enhance question list display based on user role

--- client/src/components/Questions/QuestionHome.tsx
+++ client/src/components/Questions/QuestionHome.tsx
@@ -1,17 +1,17 @@
-import { useEffect, useState } from "react";
-import { useAppDispatch, useAppSelector } from "../../hook/hooks";
+import {useEffect, useState} from "react";
+import {useAppDispatch, useAppSelector} from "../../hook/hooks";
 import AddQuestion from "./AddQuestion";
-import { fetchQuestionsAsync } from "../../features/QuestionSlice";
-import { Qualificatif, Question } from "../../types/types";
-import { RootState } from "../../api/store";
-import { fetchQualificatifsAsync } from "../../features/QualificatifSlice";
-import { typeQuestionMapper } from "../../mappers/mappers.ts";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
+import {fetchQuestionsAsync} from "../../features/QuestionSlice";
+import {Qualificatif, Question} from "../../types/types";
+import {RootState} from "../../api/store";
+import {fetchQualificatifsAsync} from "../../features/QualificatifSlice";
+import {typeQuestionMapper} from "../../mappers/mappers.ts";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPenToSquare, faTrash} from "@fortawesome/free-solid-svg-icons";
 import UpdateQuestion from "./UpdateQuestion.tsx";
 import DeleteQuestionConfirmation from "./DeleteQuestionConfirmation.tsx";
-import { FaSearch } from "react-icons/fa";
-import { MdClear } from "react-icons/md";
+import {FaSearch} from "react-icons/fa";
+import {MdClear} from "react-icons/md";
 
 const QuestionHome = () => {
   const role = localStorage.getItem('role');
@@ -117,7 +117,7 @@ const QuestionHome = () => {
       <>
         <div className="flex flex-col gap-5 items-center pt-32 mx-auto rounded-s-3xl bg-white w-full h-screen">
           <h1 className="text-xl font-bold">Liste des questions</h1>
-          <div className="flex flex-row items-center justify-between gap-5 w-[80%] px-14">
+          <div className={`flex flex-row items-center justify-between gap-5 px-14 ${role == "ADM" ? "w-[55%]" : "w-[80%]"}`}>
             <div className="w-2/3 hover:cursor-text flex flex-row items-center gap-5">
               <label className="input input-bordered flex items-center gap-2 shadow-md w-1/3">
                 <input
@@ -129,7 +129,7 @@ const QuestionHome = () => {
                 />
                 <FaSearch />
               </label>
-              <div className="flex items-center gap-3 w-1/2">
+              {role == "ENS" && <div className="flex items-center gap-3 w-1/2">
                 <select
                     className="select select-bordered grow w-full max-w-xs shadow-md"
                     value={selectedType}
@@ -144,7 +144,7 @@ const QuestionHome = () => {
                     <MdClear size={20} />
                   </button>
                 </div>
-              </div>
+              </div>}
             </div>
             <div className="tooltip" data-tip="Ajouter une question">
               <button
@@ -157,23 +157,20 @@ const QuestionHome = () => {
           </div>
 
           <div className="overflow-y-auto w-full">
-            <table className="table table-zebra w-[75%] mx-auto">
+            <table className={`table table-zebra  mx-auto ${role == "ADM" ? "w-[50%]" : "w-[80%]"}`}>
               <thead>
               <tr>
-                <th onClick={() => handleSortChange("intitule")}>
+                <th  className={"w-1/3"} onClick={() => handleSortChange("intitule")}>
                   Intitulé{" "}
                   {sortField === "intitule" &&
                       (sortOrder === "asc" ? "↑" : "↓")}
                 </th>
-                <th onClick={() => handleSortChange("idQualificatif")}>
+                <th>
                   Qualificatifs{" "}
-                  {sortField === "idQualificatif" &&
-                      (sortOrder === "asc" ? "↑" : "↓")}
                 </th>
-                <th onClick={() => handleSortChange("type")}>
-                  Type{" "}
-                  {sortField === "type" && (sortOrder === "asc" ? "↑" : "↓")}
-                </th>
+                {role == "ENS" && <th>
+                  Type
+                </th>}
                 <th className="text-center">Actions</th>
               </tr>
               </thead>
@@ -203,17 +200,19 @@ const QuestionHome = () => {
                                 " - " +
                                 question?.idQualificatif?.minimal || "N/A"}
                           </td>
-                          <td className="px-4 py-2 ">
-                            {question.type === "QUP" ? (
-                                <div className="badge badge-accent text-white">
-                                  {typeQuestionMapper(question.type)}
-                                </div>
-                            ) : (
-                                <div className="badge badge-success text-white">
-                                  {typeQuestionMapper(question.type)}
-                                </div>
-                            )}
-                          </td>
+                          {
+                              role == "ENS" && <td className="px-4 py-2 ">
+                                {question.type === "QUP" ? (
+                                    <div className="badge badge-accent text-white">
+                                      {typeQuestionMapper(question.type)}
+                                    </div>
+                                ) : (
+                                    <div className="badge badge-success text-white">
+                                      {typeQuestionMapper(question.type)}
+                                    </div>
+                                )}
+                              </td>
+                          }
 
                           <td className="flex gap-3 justify-center items-center">
                             <div className="tooltip"
