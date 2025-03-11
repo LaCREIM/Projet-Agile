ad81c0c7bdbc1549d4d98eff1d2a75eed35ea267
IDRISSI-Yassine <yassine.idrissi1@etu.uae.ac.ma>
2025-03-11 13:31:21 +0100

    refactor: enhance evaluation creation and retrieval logic with improved error handling and DTO mapping

--- backend/src/main/java/com/example/backendagile/controllers/UniteEnseignementController.java
+++ backend/src/main/java/com/example/backendagile/controllers/UniteEnseignementController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/unites-enseignement")
+@RequestMapping("/api/unites-enseignement")
 public class UniteEnseignementController {
 
     private final UniteEnseignementService uniteEnseignementService;
--- backend/src/main/java/com/example/backendagile/dto/UniteEnseignementDTO.java
+++ backend/src/main/java/com/example/backendagile/dto/UniteEnseignementDTO.java
@@ -1,27 +1,17 @@
 package com.example.backendagile.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class UniteEnseignementDTO {
     private String codeUE;
     private String designationUE;
 
-    public UniteEnseignementDTO(String codeUE, String designation) {
+    public UniteEnseignementDTO(String codeUE, String designationUE) {
         this.codeUE = codeUE;
-        this.designationUE = designation;
+        this.designationUE = designationUE;
     }
 
-    public String getCodeUE() {
-        return codeUE;
-    }
-
-    public void setCodeUE(String codeUE) {
-        this.codeUE = codeUE;
-    }
-
-    public String getDesignationUE() {
-        return designationUE;
-    }
-
-    public void setDesignationUE(String designation) {
-        this.designationUE = designation;
-    }
 }
