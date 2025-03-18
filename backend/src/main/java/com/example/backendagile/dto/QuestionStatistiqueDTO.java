3de0f3c15cb744fbfe9f2daaa7ccf50432066f2f
IDRISSI-Yassine <yassine.idrissi1@etu.uae.ac.ma>
2025-03-18 10:20:24 +0100

    add dto to stats

---
+++ backend/src/main/java/com/example/backendagile/dto/QuestionStatistiqueDTO.java
@@ -0,0 +1,21 @@
+package com.example.backendagile.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class QuestionStatistiqueDTO {
+    private Long idQuestion;
+    private Double moyennePositionnement;
+    private String maximal;
+    private String minimal;
+    private Long nbReponses;
+    private String intitule;
+    private String designation;
+    private long[] totalPositionnements;
+}
