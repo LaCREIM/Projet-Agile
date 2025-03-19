908c704642f77a215a8f7c76f1eab823b56d9610
BILALck4 <115404316+BILALck4@users.noreply.github.com>
2025-03-19 10:44:20 +0100

    Update EnseignantsHome.tsx

--- client/src/components/Enseignant/EnseignantsHome.tsx
+++ client/src/components/Enseignant/EnseignantsHome.tsx
@@ -97,7 +97,11 @@ const EnseignantsHome = () => {
 
   const updateEnseignantModalRef = useRef<HTMLDialogElement | null>(null);
   const enseignantDetailsModalRef = useRef<HTMLDialogElement | null>(null);
-
+  const formatPhoneNumber = (value: string): string => {
+    return value.replace(/\D/g, "") // Supprime tous les caractères non numériques
+                .replace(/(\d{2})(?=\d)/g, "$1 ") // Ajoute un espace tous les deux chiffres
+                .trim();
+  };
 
   useEffect(() => {
     const filtered = enseignants.filter((enseignant) => {
