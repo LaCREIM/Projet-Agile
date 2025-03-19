f116ac285a9622d533d575384fbfaa21613d4129
Sohaib Manah <sohaib.manah@etudiant.univ-brest.fr>
2025-03-19 15:14:25 +0100

    feat: update API base URL and add chartjs-plugin-annotation for enhanced chart functionality

--- client/src/api/baseUrl.ts
+++ client/src/api/baseUrl.ts
@@ -1,4 +1,4 @@
-//export const API_BASE_URL = "https://186b-2a04-cec0-1177-1b6f-7c7a-a583-1f4-d636.ngrok-free.app";
- export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "https://186b-2a04-cec0-1177-1b6f-7c7a-a583-1f4-d636.ngrok-free.app";
+//  export const API_BASE_URL = "http://localhost:8080";
 
 
